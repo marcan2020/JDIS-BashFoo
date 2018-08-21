f98f283c09f659b7c7da0c7f7ff0eface6c1d52c
marcan2020 <marcan2020@gmail.com>
2018-08-21 15:23:05 -0400

    Redirect all outputs to /dev/null

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /root/challenges && ./tmp_directory_challenge.sh &
-cd /root/challenges/challenge7/ && freeflag.py &
-cd /root/challenges/challenge8/ && python -m SimpleHTTPServer 4443 &
-cd /opt/likeasir/ && ./likeasir.pl &
+cd /root/challenges && sh tmp_directory_challenge.sh > /dev/null &
+cd /root/challenges/challenge7/ && python freeflag.py > /devnull &
+cd /root/challenges/challenge8/ && python -m SimpleHTTPServer 4443 > /dev/null &
+cd /opt/likeasir/ && ./likeasir.pl > /dev/null &
